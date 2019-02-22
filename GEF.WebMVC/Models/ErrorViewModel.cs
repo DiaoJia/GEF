1e287e4435ab5b792d6d9c22b2069c1a3e8f04ad
KevinDiao <diaojia@outlook.com>
2019-02-22 17:06:24 +0800

    initial project

---
+++ GEF.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GEF.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
